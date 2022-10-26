91361d38ab8c5956668c398dcfaa1a5eb9922ed6
Shane Tanner <shanetanner03@gmail.com>
2022-10-26 15:00:57 -0500

    Created DisplayEntries Component and Instantiated Component on App Component.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+
+function App() {
+
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 175, date: '11-23-2021'}])
+  
+  return (
+    <div >
+      <DisplayEntries parentEntries={entries} />
+    </div>
+  );
+}
+
+export default App;
